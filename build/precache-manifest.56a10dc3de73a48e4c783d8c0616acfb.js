3dd0fb6c64d4cf43f9f9234ea0fdf828ab30bdd0
hathex <50103127+hathex@users.noreply.github.com>
2019-09-16 13:15:25 +0200

    build

---
+++ build/precache-manifest.56a10dc3de73a48e4c783d8c0616acfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d49d9549b3ef20023de686f8593927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af5b30f99fa4144adbd",
+    "url": "/static/css/main.2899911c.chunk.css"
+  },
+  {
+    "revision": "7f2dbbbad130f29cb82b",
+    "url": "/static/js/2.69a22879.chunk.js"
+  },
+  {
+    "revision": "6af5b30f99fa4144adbd",
+    "url": "/static/js/main.10069c02.chunk.js"
+  },
+  {
+    "revision": "74e5b205e42929ee8095",
+    "url": "/static/js/runtime~main.ab4abc95.js"
+  },
+  {
+    "revision": "0771dc6c50581ba01a9ad6360e4bd338",
+    "url": "/static/media/bgc.0771dc6c.png"
+  }
+]);
